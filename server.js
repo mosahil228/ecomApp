61462b502d05730f6e886c534b19784a33f1df04
mosahil228 <54403198+mosahil228@users.noreply.github.com>
2024-01-09 20:47:50 +0530

    mongodb connected

---
+++ server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+// import authRoutes from "./routes/authRoute.js";
+
+//configure env
+dotenv.config();
+
+//databse config
+connectDB(); 
+
+//rest object
+const app = express();
+
+//middelwares
+app.use(express.json());
+app.use(morgan("dev"));
+
+//routes
+// app.use("/api/v1/auth", authRoutes);
+
+//rest api
+app.get("/", (req, res) => {
+  res.send("Welcome to ecommerce app");
+});
+
+//PORT
+const PORT =  process.env.PORT || 8080;
+
+//run listen
+app.listen(PORT, () => {
+  console.log(
+    "server running on port " + PORT
+  );
+});
